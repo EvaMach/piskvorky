12ebc27f192d899a2f7ab972de73d0a4ebf9eb1b
Eva Machova <evca.machova@gmail.com>
2022-04-22 20:39:21 +0200

    add play and changing turns

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const turn = 'circle';
-
 const buttons = document.querySelectorAll('.game-pad button');
+let turn = 'circle';
+let turnImage = document.querySelector('.turn-icon');
 
-for (let i = 0; i < buttons.length; i ++) {
-    buttons[i].addEventListener('click', (event) => {
-        event.target.classList.add(`board__field--${turn}`)  
-    })
-};
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', (event) => {
+    if (buttons[i].classList.length < 1) {
+      event.target.classList.add(`board__field--${turn}`, 'board__field');
+      if (turn === 'circle') {
+        turn = 'cross';
+      } else {
+        turn = 'circle';
+      }
+      turnImage.src = `pravidla_images/${turn}.svg`;
+      event.target.disabled = true;
+    }
+  });
+}
