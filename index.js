2de051b6f04104f5a99f6ff7f946b47566dc7e95
Eva Machova <evca.machova@gmail.com>
2022-05-03 23:29:50 +0200

    add game behaviour

--- index.js
+++ index.js
@@ -1,14 +1,91 @@
 const buttons = document.querySelectorAll('.game-pad button');
 let turn = 'circle';
+let winner;
 let turnImage = document.querySelector('.turn-icon');
+
+const getSymbol = (field) => {
+  return field.className.slice(14, 20);
+};
+
+const boardSize = 10;
+const getField = (row, column) => {
+  return buttons[row * boardSize + column];
+};
+
+const getPosition = (field) => {
+  let i = 0;
+  while (i < buttons.length && field !== buttons[i]) {
+    i++;
+  }
+  return {
+    row: Math.floor(i / boardSize),
+    column: i % boardSize,
+  };
+};
+
+const symbolsToWin = 5;
+const isWinningMove = (field) => {
+  const symbol = getSymbol(field);
+  const origin = getPosition(field);
+
+  let i;
+
+  let inRow = 1;
+  i = origin.column;
+  while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+    inRow++;
+    i--;
+  }
+
+  i = origin.column;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(origin.row, i + 1))
+  ) {
+    inRow++;
+    i++;
+  }
+
+  if (inRow >= symbolsToWin) {
+    return true;
+  }
+
+  let inColumn = 1;
+  i = origin.row;
+  while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+    inColumn++;
+    i--;
+  }
+
+  i = origin.row;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(i + 1, origin.column))
+  ) {
+    inColumn++;
+    i++;
+  }
+
+  if (inColumn >= symbolsToWin) {
+    return true;
+  }
+
+  return false;
+};
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', (event) => {
     if (buttons[i].classList.length < 1) {
       event.target.classList.add(`board__field--${turn}`, 'board__field');
+      event.target.disabled = true;
+      if (isWinningMove(event.target)) {
+        winner = turn === 'circle' ? 'kolečko' : 'křížek';
+        confirm(`Vyhrál ${winner}. Spustit novou hru?`)
+          ? location.reload()
+          : '';
+      }
       turn = turn === 'circle' ? 'cross' : 'circle';
       turnImage.src = `pravidla_images/${turn}.svg`;
-      event.target.disabled = true;
     }
   });
 }
